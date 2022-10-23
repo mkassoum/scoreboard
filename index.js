5a3ca6179648b21b545c625cac2e8a33333dcf12
mkassoum <mhmdkassoum@gmail.com>
2022-10-23 21:36:00 +0200

    changed the background colors and added a reset score button  Next will be adding live stats

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-let scoreH = 0 
+let scoreH = 0
 let scoreG = 0
 
 
-function addHome (int) {
+function addHome(int) {
     scoreH += int
-    document.getElementById("home-score").innerText = scoreH
-    
+    document.getElementById("home-score").textContent = scoreH
+
 }
- function addGuest (int) {
-      scoreG += int
-    document.getElementById("guest-score").innerText = scoreG
-    
+function addGuest(int) {
+    scoreG += int
+    document.getElementById("guest-score").textContent = scoreG
+
+}
+
+function reset() {
+    document.getElementById("home-score").textContent = 0
+    document.getElementById("guest-score").textContent = 0
+    scoreH = 0
+    scoreG = 0
 }
